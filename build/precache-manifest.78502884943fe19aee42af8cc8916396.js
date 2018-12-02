bd63bc722863dd40e6cb37ec68045cf6a94d73db
Sason Braha <sasonbraha@gmail.com>
2018-12-02 14:45:57 +0200

    deleted readme

--- build/precache-manifest.78502884943fe19aee42af8cc8916396.js
+++ build/precache-manifest.78502884943fe19aee42af8cc8916396.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/herolo-cinema/static/js/runtime~main.c39d6945.js"
   },
   {
-    "revision": "c4c8557061e3549d760d",
-    "url": "/herolo-cinema/static/js/main.c4c85570.chunk.js"
+    "revision": "b744523ebae3e2303def",
+    "url": "/herolo-cinema/static/js/main.b744523e.chunk.js"
   },
   {
     "revision": "64b10488c67d28b21f44",
     "url": "/herolo-cinema/static/js/1.64b10488.chunk.js"
   },
   {
-    "revision": "c4c8557061e3549d760d",
+    "revision": "b744523ebae3e2303def",
     "url": "/herolo-cinema/static/css/main.6e9a3333.chunk.css"
   },
   {
-    "revision": "8ea24e7f3b1e44d2a161e465d7af615c",
+    "revision": "d4fe31b81beb07fea9aa2ddbf5212aad",
     "url": "/herolo-cinema/index.html"
   }
 ];
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://www.omdbapi.com/?apikey=bddffa2e';
+export const API_URL = 'https://www.omdbapi.com/?apikey=bddffa2e';
