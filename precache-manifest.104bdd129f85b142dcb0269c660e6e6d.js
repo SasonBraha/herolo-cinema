1076e66f532d5856d19cc4f7f9a387ae3c624437
Sason Braha <sasonbraha@gmail.com>
2018-12-02 14:42:06 +0200

    Updates

---
+++ precache-manifest.104bdd129f85b142dcb0269c660e6e6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c39d694574e552dcfc72",
+    "url": "/herolo-cinema/static/js/runtime~main.c39d6945.js"
+  },
+  {
+    "revision": "c4c8557061e3549d760d",
+    "url": "/herolo-cinema/static/js/main.c4c85570.chunk.js"
+  },
+  {
+    "revision": "64b10488c67d28b21f44",
+    "url": "/herolo-cinema/static/js/1.64b10488.chunk.js"
+  },
+  {
+    "revision": "c4c8557061e3549d760d",
+    "url": "/herolo-cinema/static/css/main.6e9a3333.chunk.css"
+  },
+  {
+    "revision": "8ea24e7f3b1e44d2a161e465d7af615c",
+    "url": "/herolo-cinema/index.html"
+  }
+];
